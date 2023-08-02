eb4f5f83ec7a96a2aed728711e56fb6a25a94ced
alexanderstracke <alexander.stracke@gmx.net>
2023-08-02 12:06:16 +0200

    Updated pom: webflux for Webclient Created classes: Controller, Pokemon, Response, Service

---
+++ backend/src/main/java/de/neuefische/backend/Controller.java
@@ -0,0 +1,22 @@
+package de.neuefische.backend;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class Controller {
+
+    private final Service s;
+
+
+    @GetMapping
+    public List<Pokemon> getAllPokemons() {
+        return s.allPokemons();
+    }
+}
