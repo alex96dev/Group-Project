106f532ac894a4ef11ab8e407f50c971a774eb71
alexanderstracke <alexander.stracke@gmx.net>
2023-08-04 11:41:08 +0200

    api + randomiser

--- backend/src/main/java/de/neuefische/backend/Controller.java
+++ backend/src/main/java/de/neuefische/backend/Controller.java
@@ -2,6 +2,7 @@ package de.neuefische.backend;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,13 @@ public class Controller {
 
 
     @GetMapping
-    public List<Pokemon> getAllPokemons() {
+    public List<PokemonComplete> getAllPokemons() {
         return s.allPokemons();
     }
+
+    /*@GetMapping("/{id}")
+    public Pokemon getPokemonDetailsById(@PathVariable String id){
+        return s.getPokemonDetailsById(id);
+    }*/
+
 }
--- backend/src/main/java/de/neuefische/backend/Pokemon.java
+++ backend/src/main/java/de/neuefische/backend/Pokemon.java
@@ -1,8 +1,9 @@
 package de.neuefische.backend;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
 @Data
 public class Pokemon {
-private String name;
+    private String name;
+    private String url;
 }
